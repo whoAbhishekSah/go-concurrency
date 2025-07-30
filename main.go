27b57d717dfad45ac187a095e6c9dd9ec0ee4e5b
Abhishek Sah <abhi.sah.97@gmail.com>
2025-07-30 14:03:07 +0530

    count digits in words concurrently using channels

--- main.go
+++ main.go
@@ -3,28 +3,74 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"strings"
+	"sync"
+	"unicode"
 )
 
-// After sending the message to the channel ➊, 
-// goroutine B gets blocked. 
-// Only when goroutine A receives the message ➌ does 
-// goroutine B continue and print "message sent" ➋.
-func main() {
-    messages := make(chan string)
+// counter stores the number of digits in each word.
+// The key is the word, and the value is the number of digits.
+type counter map[string]int
+
+// solution start
+
+// countDigitsInWords counts the number of digits in the words of a phrase.
+func countDigitsInWords(phrase string) counter {
+    words := strings.Fields(phrase)
+    counted := make(chan int)
 
     go func() {
-        fmt.Println("B: Sending message...")
-        messages <- "ping"                    // (1)
-        fmt.Println("B: Message sent!")       // (2)
+        for _, word := range words {
+			count := countDigits(word)
+			counted <-count
+		}
     }()
+	
+	stats := make(map[string]int)
+	for _, word := range words {
+		stats[word] = <-counted
+	} 
+    // Read values from the counted channel
+    // and fill stats.
 
-    fmt.Println("A: Doing some work...")
-    time.Sleep(500 * time.Millisecond)
-    fmt.Println("A: Ready to receive a message...")
+    // As a result, stats should contain words
+    // and the number of digits in each.
 
-    <-messages                               //  (3)
+    return stats
+}
 
-    fmt.Println("A: Messege received!")
-    time.Sleep(100 * time.Millisecond)
+// solution end
+
+// countDigits returns the number of digits in a string.
+func countDigits(str string) int {
+	count := 0
+	for _, char := range str {
+		if unicode.IsDigit(char) {
+			count++
+		}
+	}
+	return count
+}
+
+// asStats converts statistics from sync.Map to a regular map.
+func asStats(m *sync.Map) counter {
+	stats := counter{}
+	m.Range(func(word, count any) bool {
+		stats[word.(string)] = count.(int)
+		return true
+	})
+	return stats
+}
+
+// printStats prints the number of digits in words.
+func printStats(stats counter) {
+	for word, count := range stats {
+		fmt.Printf("%s: %d\n", word, count)
+	}
+}
+
+func main() {
+	phrase := "0ne 1wo thr33 4068"
+	counts := countDigitsInWords(phrase)
+	printStats(counts)
 }
