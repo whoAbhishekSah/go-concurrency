cd78e4ced56079f07c7d14f531298338a3d1c3ce
Abhishek Sah <abhi.sah.97@gmail.com>
2025-12-10 15:59:23 +0530

    gather without sharing same slice with multiple goroutines

--- async.go
+++ async.go
@@ -13,31 +13,43 @@ func await(fn func() any) any {
 	return <-out
 }
 
+type result struct {
+	idx int
+	val any
+}
+
 // gather runs all passed functions concurrently
 // and returns the results when they are ready.
 func gather(funcs []func() any) []any {
+	results := make(chan result, len(funcs))
 	res := make([]any, len(funcs))
-	done := make(chan struct{})
-	for idx, fn := range funcs{
-		go func(){
-			res[idx] = fn()
-			done <- struct{}{}
+	for idx, fn := range funcs {
+		go func() {
+			results <- result{idx, fn()}
 		}()
 	}
 	for range funcs {
-		<-done
+		item := <-results
+		res[item.idx] = item.val
 	}
 	return res
 }
 
+// squared returns a function that returns
+// the square of the input number.
+func squared(n int) func() any {
+	return func() any {
+		return n * n
+	}
+}
 
 func main() {
-	slowpoke := func() any {
-		fmt.Print("I'm so..& ")
-		time.Sleep(100 * time.Millisecond)
-		fmt.Println("slow")
-		return "okay"
-	}
-	res := await(slowpoke)
-	fmt.Println(res.(string))
+	funcs := []func() any{squared(2), squared(3), squared(4)}
+
+	start := time.Now()
+	nums := gather(funcs)
+	elapsed := float64(time.Since(start)) / 1_000_000
+
+	fmt.Println(nums)
+	fmt.Printf("Took %.0f ms\n", elapsed)
 }
