eae8b3a0705aeca75465519e6eefa5863ffe3e13
Abhishek Sah <abhi.sah.97@gmail.com>
2026-01-07 12:08:30 +0530

    handle context cancellation

--- etl.go
+++ etl.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"os"
 	"strings"
@@ -14,18 +15,18 @@ const (
 	MaxWorker      = 3
 )
 
-func aggregateLog(filepath string) int {
-	pending := reader(filepath) // return a channel where each line is written line by line
-	collector := transformDispatcher(pending)
+func aggregateLog(ctx context.Context, filepath string) int {
+	pending := reader(ctx, filepath) // return a channel where each line is written line by line
+	collector := transformDispatcher(ctx, pending)
 	return sinker(collector) // counts the error lines
 }
 
-func transformDispatcher(pending <-chan string) <-chan string {
+func transformDispatcher(ctx context.Context, pending <-chan string) <-chan string {
 	collector := make(chan string)
 	wg := &sync.WaitGroup{}
 	for i := range MaxWorker {
 		wg.Add(1)
-		go transformer(i, pending, collector, wg)
+		go transformer(ctx, i, pending, collector, wg)
 	}
 	go func() {
 		wg.Wait()
@@ -34,37 +35,60 @@ func transformDispatcher(pending <-chan string) <-chan string {
 	return collector
 }
 
-func reader(filepath string) <-chan string {
+// context cancellation must be checked each time we scan a new line
+func reader(ctx context.Context, filepath string) <-chan string {
 	pending := make(chan string)
 	go func() {
 		file, err := os.Open(filepath)
 		if err != nil {
-			panic(fmt.Errorf("Failed to open file %s, err: %v", filepath, err))
+			fmt.Printf("Failed to open file %s, err: %v\n", filepath, err)
+			return
 		}
 		defer file.Close()
 		scanner := bufio.NewScanner(file)
 		for scanner.Scan() {
 			line := scanner.Text()
-
-			pending <- line
+			select {
+			case pending <- line:
+			case <-ctx.Done():
+				fmt.Println("Context error in reader")
+				return
+			}
 		}
 		if err := scanner.Err(); err != nil {
-			panic(fmt.Errorf("scan error %v", err))
+			fmt.Printf("scan error %v\n", err)
 		}
 		close(pending)
 	}()
 	return pending
 }
 
-func transformer(id int, pending <-chan string, collector chan<- string, wg *sync.WaitGroup) {
+// caveat: loss of order of logs
+// The sinker could receive old line before new lines.
+// context cancellation must be checked each time we read from channel
+func transformer(ctx context.Context, id int, pending <-chan string, collector chan<- string, wg *sync.WaitGroup) {
 	fmt.Printf("staring worker %d\n", id)
 	defer wg.Done()
-	for checkStr := range pending {
-		if strings.Contains(checkStr, ErrMatchSubstr) {
-			// Simulate expensive processing (e.g., resizing an image)
-			collector <- checkStr
-			time.Sleep(500 * time.Millisecond)
-			fmt.Printf("Worker %d finished str processing\n", id)
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Printf("Context error in transformer while reading from pending, worker id %d\n", id)
+			return
+		case checkStr, ok := <-pending:
+			if !ok {
+				return
+			}
+			if strings.Contains(checkStr, ErrMatchSubstr) {
+				// Simulate expensive processing (e.g., resizing an image)
+				time.Sleep(500 * time.Millisecond)
+				fmt.Printf("Worker %d finished str processing\n", id)
+				select {
+				case <-ctx.Done():
+					fmt.Printf("Context error in transformer while sending to collector, worker id %d\n", id)
+					return
+				case collector <- checkStr:
+				}
+			}
 		}
 	}
 }
@@ -81,5 +105,6 @@ func sinker(collector <-chan string) int {
 }
 
 func main() {
-	fmt.Println(aggregateLog("./app.log"))
+	ctx, _ := context.WithTimeout(context.Background(), 100*time.Millisecond)
+	fmt.Println(aggregateLog(ctx, "./app.log"))
 }
